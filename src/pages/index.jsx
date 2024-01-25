c8e9d382497c226195a7dd149b336331b5de2dda
lutfiandri <lutfiandri232@gmail.com>
2024-01-25 11:37:41 +0700

    feat: add navigation from home to device plot

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,9 +2,11 @@ import BaseLayout from '@/components/layout/BaseLayout';
 import { useEffect, useState } from 'react';
 import { db } from '@/services/firebase';
 import { collection, onSnapshot, query } from 'firebase/firestore';
+import { useNavigate } from 'react-router-dom';
 
 function HomePage() {
   const [devices, setDevices] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const q = query(collection(db, 'devices'));
@@ -28,6 +30,7 @@ function HomePage() {
         {devices.map((device) => (
           <div
             key={device.id}
+            onClick={() => navigate('/' + device.id)}
             role="button"
             className="w-52 h-32 grid place-items-center bg-blue-200 rounded-lg hover:bg-blue-300 transition-all duration-200"
           >
